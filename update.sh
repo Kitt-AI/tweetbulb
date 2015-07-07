f9c8fe45a29a68770fb30bf7729e1b48bed390b1
yaoxuchen <yaoxuchen@gmail.com>
2015-07-07 15:54:43 -0700

    update

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ wget kitt.ai/tweetbulb0 -O index.html
 sed -i 's/"\/assets/"http:\/\/kitt.ai\/assets/g' index.html
 sed -i 's/62235408-1/62235408-3/g' index.html
 
-git commit -a
+git commit -am 'update'
 git push
